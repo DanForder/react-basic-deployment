643f6806642838fedf409f44c38cf3ecf12eeb06
Dan Forder <danfrdr@gmail.com>
2022-01-05 11:30:52 +0000

    initial create react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Deployed with GitHub Pages</h1>
+      <img src="https://placedog.net/300" alt="cute doggo" />
+    </div>
+  );
+};
+
+export default App;
